c4d242409791c75e889f10dce6d0512b6d012c16
Ryusei OYA <ryuryusei401@toki.waseda.jp>
2021-01-08 23:23:25 +0900

    MARU-1: create new file

---
+++ static/index.js
@@ -0,0 +1,12 @@
+// Bootstrapのスタイルシート側の機能を読み込む
+// webpack.config.jsでcssを読み取れるようにへんこうを加える　
+import "bootstrap/dist/css/bootstrap.min.css";
+// BootstrapのJavaScript側の機能を読み込む
+import "bootstrap";
+// fontawesomeを使う設定
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+
+
+import "./scss/style.scss";
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const smp = new SpeedMeasurePlugin()
+
+
+module.exports = smp.wrap({
+  // modeはproduction/developmentで記述
+  // ""で囲むことに注意
+  mode: "development",
+  // どのファイルを読み込むか　default=> ./src/index.js
+  entry: './static/index.js',
+
+  //　entryで読み込んだファイルのコンパイルの吐き出し場所
+  // フロント開発時
+  // output: {
+  //   path: path.resolve(__dirname, 'templates'),
+  //   // distにsample.jsというファイル名で吐き出し
+  //   filename: 'main.js',
+  // },
+
+  // flask実行時
+  output: {
+    path: path.resolve(__dirname, 'static/js'),
+    // distにsample.jsというファイル名で吐き出し
+    filename: 'main.js',
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname,"templates"),
+    host: '0.0.0.0',
+    hot : true,
+    open : true,
+    port: 9005
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+
+      // Sassファイルの読み込みとコンパイル
+      {
+        // 拡張子がsassとscssのファイルを対象とする
+        test: /\.s[ac]ss$/i,
+        // ローダー名
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+       // 対象となるファイルの拡張子
+        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+        // 画像をBase64として取り込む
+        type: "asset/inline",
+      }
+      // bootstrapを読み取れるように追加
+    ],
+  },
+  // ES5(IE11等)向けの指定（webpack 5以上で必要）
+  // target: ["web", "es5"],
+});
