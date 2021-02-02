d3101110f47556902323e2fba96037f69cdd1458
Ryusei OYA <ryuryusei401@toki.waseda.jp>
2021-02-02 11:49:54 +0900

    herokuへのデプロイの確認

--- static/js/component/form.js
+++ static/js/component/form.js
@@ -1,4 +1,23 @@
+const title = document.getElementById("js-title");
+let downloadButton = $(".js-submit-download");
+let getButton = $(".js-submit-get");
 
-
-
-
+// domの読み込み時に実行
+window.onload = function () {
+    let titleText = title.innerText;
+    console.log(titleText);
+    // テキストがCompliteだったら Get button をdownloadに変更する
+    if (titleText === "Complete") {
+        console.log("after");
+        downloadButton.addClass("js-submit-show");
+        console.log(getButton.css("display"));
+        getButton.addClass("js-submit-hide");
+        console.log(downloadButton.css("display"));
+    } else {
+        console.log("before");
+        getButton.addClass("js-submit-show");
+        console.log(getButton.css("display"));
+        downloadButton.addClass("js-submit-hide");
+        console.log(downloadButton.css("display"));
+    }
+};
